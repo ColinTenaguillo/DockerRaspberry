bc0cc83cfacdc2de7fc33936efa7353a7135bc04
Colin <Colin.tenaguillo@orange.fr>
2021-04-23 14:47:32 +0200

    fix script

--- docker.sh
+++ docker.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
+# docker.sh
 
-if [$1 = influxdb]
+if [ $1 == 'influxdb' ] || [ $1 == 'all' ]
 then
   docker build -f Dockerfile.influxdb . -t pi/influxdb:latest
 fi
 
-if [$1 = influxdb]
+if [ $1 == 'api' ] || [ $1 == 'all' ]
 then
-  docker build -f Dockerfile.influxdb . -t pi/influxdb:latest
+  docker build -f Dockerfile . -t pi/api:latest
 fi
 
-if [$1 = influxdb]
+if [ $1 == 'dev' ] || [ $1 == 'all' ]
 then
-  docker build -f Dockerfile.influxdb . -t pi/influxdb:latest
+  docker build -f Dockerfile.dev . -t pi/dev:latest
 fi
 
-if [$1 = influxdb]
+if [ $1 == 'datacollector' ] || [ $1 == 'all' ]
 then
-  docker build -f Dockerfile.influxdb . -t pi/influxdb:latest
+  docker build -f Dockerfile.datacollector . -t pi/datacollector:latest
 fi
-
-if [$1 = influxdb]
-then
-  docker build -f Dockerfile.influxdb . -t pi/influxdb:latest
-fi
-docker build -f Dockerfile . -t pi/api:latest
-docker build -f Dockerfile.dev . -t pi/dev:latest
-docker build -f Dockerfile.datacollector . -t pi/datacollector:latest
