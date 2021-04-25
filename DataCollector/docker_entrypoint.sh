6d0c9d78a2521ac99eaeedc31b4e6047ec3f628f
Colin <Colin.tenaguillo@orange.fr>
2021-04-25 18:52:44 +0200

    waiting for dbus loop script

--- DataCollector/docker_entrypoint.sh
+++ DataCollector/docker_entrypoint.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-service dbus start
-bluetoothd &
+SERVICE=dbus;
+STARTED=false;
 
-sleep 30
+timeout=6  # timeout after 1mn + check  if dbus is running 
+while ((timeout > 0))
+do
+  if ps ax | grep -v grep | grep $SERVICE > /dev/null
+  then
+      echo "$SERVICE service running, everything is fine"
+      break
+  else
+      echo "$SERVICE is not running"
+      if [ "$STARTED" = false ]
+      then
+        service dbus start
+        bluetoothd &
+        STARTED=true
+        echo "$SERVICE started"
+      fi
+  fi
+  sleep 10
+  ((timeout -= 1))
+done 
 
 python3 /DataCollector/DataCollector.py
